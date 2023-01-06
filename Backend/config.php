3abc405f44d9ed3ea4221b83921d2c5900017014
arogina <rogina.alan@gmail.com>
2023-01-06 22:44:56 +0100

    Updating files

--- Backend/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define("DB_HOST", "air-mbanking.mysql.database.azure.com");
-    define("DB_USER", "mbanking");
-    define("DB_PASSWORD", "airProject123");
-    define("DB_NAME", "mbanking");

4216f681cd17b48a0652f5c392698615321af1b3
Alan Rogina <rogina.alan@gmail.com>
2024-05-21 22:05:42 +0200

    Ažuriran Gradle na 8.1.1.

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "hr.foi.air.mbankingapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hr.foi.air.mbankingapp"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
